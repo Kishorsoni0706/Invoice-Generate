174c7c35220ab0b37b7e3b4c22be2733a8032aee
Kishorsoni0706 <kishorsoni0706@gmail.com>
2024-09-07 14:42:04 +0530

    Minor change for handle date in format.

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -5,19 +5,29 @@ namespace App\Http\Controllers;
 use App\Models\Invoice;
 use App\Models\Payment;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class InvoiceController extends Controller
 {
-    public function index()
-    {
-        $invoices = auth()->user()->invoices; // Assuming you have a relationship set up
-        return view('invoices.index', compact('invoices'));
-    }
+public function index()
+{
+    // Fetch invoices for the authenticated user
+    $invoices = auth()->user()->invoices;
+
+    // Convert the due_date to a Carbon instance for each invoice
+    $invoices = $invoices->map(function ($invoice) {
+        // Check if due_date is not already a Carbon instance and convert if necessary
+        $invoice->due_date = Carbon::parse($invoice->due_date);
+        return $invoice;
+    });
+    return view('invoices.index', compact('invoices'));
+}
 
     public function show($id)
     {
         $invoice = Invoice::findOrFail($id);
-        return view('invoices.show', compact('invoice'));
+        $invoice->due_date = Carbon::parse($invoice->due_date);
+       return view('invoices.show', compact('invoice'));
     }
 
     public function pay(Request $request, $id)
